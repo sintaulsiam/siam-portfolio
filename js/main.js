daf5338745a731221674e85478a0ae1ae37aec71
Sintaul Mahdi Siam <sintaulsiam@gmail.com>
2021-04-24 15:39:16 +0600

    home banner and navbar created stable version

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".navbar");
+
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("show-menu");
+});
